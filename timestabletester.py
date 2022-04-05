b3266c510915ab3257f2205dc9981934eccd557f
Jason Wong <89881227+JasonW2021@users.noreply.github.com>
2022-04-05 20:27:25 +1200

    Generate new random questions

--- timestabletester.py
+++ timestabletester.py
@@ -1,20 +1,33 @@
 #
 from tkinter import *
+import random
 
 class TimesTableTester:
     def __init__(self, parent):
-        Question = Label(parent, text="9 * 3 = ")
-        Question.grid(column=0,row=0,sticky=NW, padx=50, pady=10)
+        self.Question = Label(parent, text="")
+        self.Question.grid(column=0,row=0,sticky=NW, padx=50, pady=10)
 
-        CheckAnsBtn = Button(parent, text="Check Answer")
-        CheckAnsBtn.grid(column=0,row=1, sticky=SW, padx=40)
+        self.CheckAnsBtn = Button(parent, text="Check Answer")
+        self.CheckAnsBtn.grid(column=0,row=1, sticky=SW, padx=40)
 
-        InputAns = Entry(parent, width=5)
-        InputAns.grid(column=1,row=0,sticky=NE, pady=10)
+        self.InputAns = Entry(parent, width=5)
+        self.InputAns.grid(column=1,row=0,sticky=NE, pady=10)
 
-        Next = Button(parent, text="Next")
-        Next.grid(column=1,row=1, sticky=SE)
+        self.Next = Button(parent, text="Next", command=self.change)
+        self.Next.grid(column=1,row=1, sticky=SE)
 
+        self.CheckAns = Label(parent, text="")
+        self.CheckAns.grid(column=1,row=2)
+
+        self.rand1 = (random.randint(1,10))
+        self.rand2 = (random.randint(1,10))
+        self.Question.configure(text="{} * {} =".format(str(self.rand1), str(self.rand2)))
+
+    def change(self):
+        self.rand1 = (random.randint(1,10))
+        self.rand2 = (random.randint(1,10))
+        self.Question.configure(text="{} * {} =".format(str(self.rand1), str(self.rand2)))
+            
 #main rountine
 root = Tk()
 root.geometry("250x100")
